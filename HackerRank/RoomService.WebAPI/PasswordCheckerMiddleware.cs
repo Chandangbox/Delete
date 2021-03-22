cfcc295ec143e800ac35fdbec81b0fba4b5afcc2
Chandan <chandangbox@gmail.com>
2021-03-22 19:18:10 +0530

    HackerRankCore

---
+++ HackerRank/RoomService.WebAPI/PasswordCheckerMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace RoomService.WebAPI
+{
+    public class PasswordCheckerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public PasswordCheckerMiddleware(RequestDelegate next)
+        {
+			 _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+			if (!context.Request.Headers.Keys.Contains("passwordKey"))
+            {
+                context.Response.StatusCode = 400; //Bad Request                
+                await context.Response.WriteAsync("User Key is missing");
+                return;
+            }
+            else
+            {
+				string value = context.Request.Headers["passwordKey"];
+                if(value !="passwordKey123456789")
+                {
+                    context.Response.StatusCode = 401; //UnAuthorized
+                    await context.Response.WriteAsync("Invalid User Key");
+                    return;
+                }
+            }
+
+            await _next.Invoke(context);
+        
+        }
+    }
+}
